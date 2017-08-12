734a7c8b523fd567887b971fc31c4980ae67f666
olegsugatov <oleg.sugatov@gmail.com>
2017-08-12 20:31:59 +0300

    +ngMock

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
-///////////////////// 18 http
+///////////////////// 19 ng-mocks
 
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngMockE2E']);
+
+app.run(function ($httpBackend) {
+	// console.log('RUN APP');
+	var books = [
+		{
+			name: 'AngularJS'
+		},
+		{
+			name: 'EmberJS'
+		}
+	];
+
+	$httpBackend.whenGET('http://localhost:3001/books').respond(200, books);
+
+	$httpBackend.whenPOST('http://localhost:3001/books').respond(function (method, url, data) {
+		var result = JSON.parse(data);
+		books.push(result);
+		return [200, result];
+		// console.log('method', method);
+		// console.log('url', url);
+		// console.log('data', data);
+	});
+});
+
 app.controller('mainCtrl', function ($http, $scope){
 	// console.log('mainCtrl');
 	$http.get('http://localhost:3001/books')
@@ -25,6 +49,34 @@ app.controller('mainCtrl', function ($http, $scope){
 				});
 		};
 });
+
+///////////////////// 18 http
+
+// var app = angular.module('app', []);
+// app.controller('mainCtrl', function ($http, $scope){
+// 	// console.log('mainCtrl');
+// 	$http.get('http://localhost:3001/books')
+// 		.success(function (result) {
+// 			console.log('sucess', result);
+// 			$scope.books = result;
+// 		})
+// 		.error(function (result) {
+// 			console.log('error');
+// 		});
+
+// 		$scope.addBook = function (book) {
+// 			console.log(book);
+// 			$http.post('http://localhost:3001/books', book)
+// 				.success(function (result) {
+// 					console.log('Book successfully saved to backend');
+// 					$scope.books.push(book);
+// 					$scope.book = null;
+// 				})
+// 				.error(function (result){
+// 					console.log('Error in book post');
+// 				});
+// 		};
+// });
 
 ///////////////////// 17 transclude element
 
